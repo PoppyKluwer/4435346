65e67d0963b0d5fd5e60f1e15909ed926fd3204d
PoppyKluwer <jv.vittormanhaes26@gmail.com>
2024-05-26 13:32:12 -0300

    Código finalizado do exercício de emulação de uma conta bancária proposto pela DIO em uma das tarefas do BootCamp com finalidade de explorar os conhecimentos aprendidos nas últimas aulas

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import javax.print.DocFlavor.STRING;
+import java.util.Locale;
+import java.util.Scanner; 
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        String usuario = "MÁRIO ANDRADE";
+        double saldo = 237.48;
+        
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Olá! Para começarmos, por favor, digite o número da Agência");
+            String agenciaDaConta = scanner.next();
+
+        System.out.println("Agora para prosseguirmos, por favor insira o número da conta");
+            int numeroDaConta = scanner.nextInt();
+
+        System.out.println("Olá " +  usuario + ", obrigado por criar uma conta em nosso banco, sua agência é " + agenciaDaConta + ", conta " + numeroDaConta + " e seu saldo " + saldo + " já está disponível para saque");
+
+        
     }
 }
